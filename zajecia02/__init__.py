eee3372740d262697b0bb461a8e217f689318efd
Nabramowicz <97061585+Nabramowicz@users.noreply.github.com>
2024-03-27 13:27:07 +0100

    Revert "zadania02"

--- zajecia02/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ['licznik_stanu']
--- zajecia02/licznik_stanu/licznik4.py
+++
@@ -1,8 +0,0 @@
-#wersja jako atrybut funkcji
-def licznik4() -> int:
-    #jeśli atrybut istnieje to defaultowo ma wartośc 0, jeśli istnieje to wartośc zostanie zwiększona o 1
-    licznik4.licznik_stanu = getattr(licznik4, 'licznik_stanu', 0) + 1
-    return licznik4.licznik_stanu
-
-# for _ in range(10):
-#     print(licznik4()) 
